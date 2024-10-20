1a8fd1a86d45a6aae925e70350ff7a388a03df9d
yvltc <joaopeixoto30@gmail.com>
2024-10-20 01:43:57 +0100

    constructor try again

--- libraries/AP_CustomControl/AP_CustomControl_INDI.h
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.h
@@ -22,93 +22,95 @@ public:
     void update(void);
 
     // initialise variables
-    u_0.x = 0;
-    u_0.y = 0;
-    u_0.z = 0;
-
-    // saturation limits
-    ddmax = 30*M_PI;
-    dtmax = 1;
-    dtmin = 0.1;
-
-    // SOD - each vector is a row
-    SOD_A.a = {0.846153846153846,0.003076923076923,0};
-    SOD_A.b = {-30.769230769230770,-0.384615384615385,0};
-    SOD_A.c = {0,0,0};
-    SOD_B.a = {30.769230769230680,0,0};
-    SOD_B.b = {-1.384615384615385e+04,0,0};
-    SOD_B.c = {0,0,0};
-    SOD_C.a = {0.923076923076923,0.001538461538462,0};
-    SOD_C.b = {0,0,0};
-    SOD_C.c = {0,0,0};
-    SOD_D.a = {15.384615384615340,0,0};
-    SOD_D.b = {0,0,0};
-    SOD_D.c = {0,0,0};
-
-    // initial values for SOD state vectors
-    xSOD_p.x = 0;
-    xSOD_p.y = 0;
-    xSOD_p.z = 0;
-    xSOD_q.x = 0;
-    xSOD_q.y = 0;
-    xSOD_q.z = 0;
-    xSOD_u.x = 0;
-    xSOD_u.y = 0;
-    xSOD_u.z = 0;
-
-    // command filter
-    _CF_A = 0.3333;
-    _CF_B = 0.6667;
-    _CF_C = 0.6667;
-    _CF_D = 0.3333;
-
-    _CFu_A = 0.9048;
-    _CFu_B = 0.09524;
-    _CFu_C = 0.9524;
-    _CFu_D = 0.04762;
-
-    // initial values for CF state vectors
-    xCF.x = 0;
-    xCF.y = 0;
-    xCF.z = 0;
-
-    CF_A.a = {_CF_A,0,0};
-    CF_A.b = {0,_CF_A,0};
-    CF_A.c = {0,0,_CFu_A};
-    CF_B.a = {_CF_B,0,0};
-    CF_B.b = {0,_CF_B,0};
-    CF_B.c = {0,0,_CFu_B};
-    CF_C.a = {_CF_C,0,0};
-    CF_C.b = {0,_CF_C,0};
-    CF_C.c = {0,0,_CFu_C};
-    CF_D.a = {_CF_D,0,0};
-    CF_D.b = {0,_CF_D,0};
-    CF_D.c = {0,0,_CFu_D};
-
-    // valores a afinar
-    lambda = 0.6;
-    Kff = 140;
-    Ktt = 50;
-    KVt = 0.02;
-    Kp = 55;
-    Kq = 10;
-
-    // cada vetor é uma linha
-    // afinar valores depois
-    if (is_gliding)
     {
-        invG.a = {-864.9306,864.9306,0};
-        invG.b = {-103.2060,-104.6034,0};
-        invG.c = {0,0,1};
-    }
-    else
-    {
-        invG.a = {-397.6476,397.6476,-6.1354};
-        invG.b = {-47.4484,-47.4484,-0.0598};
-        invG.c = {0.4008,0.4008,6.2293};
-    }
+        u_0.x = 0;
+        u_0.y = 0;
+        u_0.z = 0;
 
-    invert_G = invG.invert();
+        // saturation limits
+        ddmax = 30*M_PI;
+        dtmax = 1;
+        dtmin = 0.1;
+
+        // SOD - each vector is a row
+        SOD_A.a = {0.846153846153846,0.003076923076923,0};
+        SOD_A.b = {-30.769230769230770,-0.384615384615385,0};
+        SOD_A.c = {0,0,0};
+        SOD_B.a = {30.769230769230680,0,0};
+        SOD_B.b = {-1.384615384615385e+04,0,0};
+        SOD_B.c = {0,0,0};
+        SOD_C.a = {0.923076923076923,0.001538461538462,0};
+        SOD_C.b = {0,0,0};
+        SOD_C.c = {0,0,0};
+        SOD_D.a = {15.384615384615340,0,0};
+        SOD_D.b = {0,0,0};
+        SOD_D.c = {0,0,0};
+
+        // initial values for SOD state vectors
+        xSOD_p.x = 0;
+        xSOD_p.y = 0;
+        xSOD_p.z = 0;
+        xSOD_q.x = 0;
+        xSOD_q.y = 0;
+        xSOD_q.z = 0;
+        xSOD_u.x = 0;
+        xSOD_u.y = 0;
+        xSOD_u.z = 0;
+
+        // command filter
+        _CF_A = 0.3333;
+        _CF_B = 0.6667;
+        _CF_C = 0.6667;
+        _CF_D = 0.3333;
+
+        _CFu_A = 0.9048;
+        _CFu_B = 0.09524;
+        _CFu_C = 0.9524;
+        _CFu_D = 0.04762;
+
+        // initial values for CF state vectors
+        xCF.x = 0;
+        xCF.y = 0;
+        xCF.z = 0;
+
+        CF_A.a = {_CF_A,0,0};
+        CF_A.b = {0,_CF_A,0};
+        CF_A.c = {0,0,_CFu_A};
+        CF_B.a = {_CF_B,0,0};
+        CF_B.b = {0,_CF_B,0};
+        CF_B.c = {0,0,_CFu_B};
+        CF_C.a = {_CF_C,0,0};
+        CF_C.b = {0,_CF_C,0};
+        CF_C.c = {0,0,_CFu_C};
+        CF_D.a = {_CF_D,0,0};
+        CF_D.b = {0,_CF_D,0};
+        CF_D.c = {0,0,_CFu_D};
+
+        // valores a afinar
+        lambda = 0.6;
+        Kff = 140;
+        Ktt = 50;
+        KVt = 0.02;
+        Kp = 55;
+        Kq = 10;
+
+        // cada vetor é uma linha
+        // afinar valores depois
+        if (is_gliding)
+        {
+            invG.a = {-864.9306,864.9306,0};
+            invG.b = {-103.2060,-104.6034,0};
+            invG.c = {0,0,1};
+        }
+        else
+        {
+            invG.a = {-397.6476,397.6476,-6.1354};
+            invG.b = {-47.4484,-47.4484,-0.0598};
+            invG.c = {0.4008,0.4008,6.2293};
+        }
+
+        invert_G = invG.invert();
+    }
 
     // user settable parameters
     static const struct AP_Param::GroupInfo var_info[];
