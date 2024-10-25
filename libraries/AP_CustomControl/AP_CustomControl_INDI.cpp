1e66399e37ee37db385504ff8b5b9bc63296db65
yvltc <joaopeixoto30@gmail.com>
2024-10-25 17:03:02 +0100

    debug pitch message

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -145,6 +145,9 @@ float AP_CustomControl_INDI::get_pitch_out(float pitch_target)
     // we don't need to do anything else
 
     //gcs().send_text(MAV_SEVERITY_INFO, "pitch INDI custom controller working");
+    char buffer[50];  // Create a buffer to hold the formatted message
+    snprintf(buffer, sizeof(buffer), "pitch INDI custom controller working, u_0[1]: %.2f", u_0[1]);
+    gcs().send_text(MAV_SEVERITY_INFO, buffer);  // Send the formatted message
 
     // return what ArduPlane main controller outputted
     // return SRV_Channels::get_output_scaled(SRV_Channel::k_elevator);
