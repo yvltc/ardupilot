564cb6d1f084a8cbb9c6eca0ae50a37640f3deb8
yvltc <joaopeixoto30@gmail.com>
2024-10-26 19:20:30 +0100

    Vt debug message

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -214,7 +214,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     bool use_TAS = _ahrs.airspeed_estimate_true(Vt);
 
     char buffer[80];  // Create a buffer to hold the formatted message
-    snprintf(buffer, sizeof(buffer), "phi: %.4f", phi);
+    snprintf(buffer, sizeof(buffer), "Vt: %.4f", Vt);
     gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
 
     if (!use_TAS)
