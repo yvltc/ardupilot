15cd855e0522249347586bb812438f8ad5147244
yvltc <joaopeixoto30@gmail.com>
2024-10-26 18:48:54 +0100

    invG

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -206,7 +206,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     invert_G = invG.invert();
 
     char buffer[80];  // Create a buffer to hold the formatted message
-    snprintf(buffer, sizeof(buffer), "invG_11: %.2f", G.a.x);
+    snprintf(buffer, sizeof(buffer), "invG_11: %.2f", invG.a.x);
     gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
 
     Vector3f angular_rates = _ahrs.get_gyro_latest();
