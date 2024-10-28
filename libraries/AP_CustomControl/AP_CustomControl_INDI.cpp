73e7b7d48461b79bac90ce86489369bf245e5085
yvltc <joaopeixoto30@gmail.com>
2024-10-28 17:09:14 +0000

    Vt control output

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -265,9 +265,9 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     u.x = u_0.x + du.x;
     u.y = u_0.y + du.y;
 
-    // char buffer[80];  // Create a buffer to hold the formatted message
-    // snprintf(buffer, sizeof(buffer), "u_0: %.4f %.4f", u_0.x, u_0.y);
-    // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
+    char buffer[80];  // Create a buffer to hold the formatted message
+    snprintf(buffer, sizeof(buffer), "u_0 Vt: %.4f", u_0.z);
+    gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
 
     saturate(-0.9*ddmax, 0.9*ddmax, &u.x);
     saturate(-0.9*ddmax, 0.9*ddmax, &u.y);
