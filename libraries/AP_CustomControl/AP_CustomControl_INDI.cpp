7f15b6d7d2b2f3a88c1ef8030a21a876b87ca367
yvltc <joaopeixoto30@gmail.com>
2024-10-28 17:24:49 +0000

    pitch debug message

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -266,7 +266,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     u.y = u_0.y + du.y;
 
     char buffer[80];  // Create a buffer to hold the formatted message
-    snprintf(buffer, sizeof(buffer), "u_0 Vt: %.4f", u_0.z);
+    snprintf(buffer, sizeof(buffer), "u_0 elevator: %.4f", u_0.y);
     gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
 
     saturate(-0.9*ddmax, 0.9*ddmax, &u.x);
