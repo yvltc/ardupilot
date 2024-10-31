1facebbc407e82010c9900c8f0b014de4ae96b61
yvltc <joaopeixoto30@gmail.com>
2024-10-31 01:40:46 +0000

    error and niu debug message

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -279,6 +279,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     float arspd_target = _tecs.get_TAS_demand();
 
     //debug
+    char buffer[80];  // Create a buffer to hold the formatted message
     roll_target = 3000;
     pitch_target = 0;
     arspd_target = 20;
@@ -300,6 +301,9 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     niu.x = Kff*error[0] - Kp*p;
     niu.y = Ktt*error[1] - Kq*q; 
     niu.z = KVt*error[2];
+
+    snprintf(buffer, sizeof(buffer), "roll error, niu: %.4f %.4f", error.x, niu.x);
+    gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
 
     du.x = invG.a.x*lambda*(niu.x - p_dot) + invG.a.y*lambda*(niu.y - q_dot) + invG.a.z*lambda*(niu.z - Vt_dot);
     du.y = invG.b.x*lambda*(niu.x - p_dot) + invG.b.y*lambda*(niu.y - q_dot) + invG.b.z*lambda*(niu.z - Vt_dot);
@@ -327,10 +331,10 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     // command filter
     sspace(u, xCF, CF_A, CF_B, CF_C, CF_D, &u, &xCF);
 
-    char buffer[80];  // Create a buffer to hold the formatted message
+    
     snprintf(buffer, sizeof(buffer), "u_0 du u: %.4f %.4f %.4f", u_0.x, du.x, u.x);
     gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
-    
+
     u_0.x = u.x;
     u_0.y = u.y;
     u_0.z = u.z;
