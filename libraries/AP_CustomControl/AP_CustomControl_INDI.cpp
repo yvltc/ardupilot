ac67321f282d95bbc147fc8f0b917c082e03c89d
yvltc <joaopeixoto30@gmail.com>
2024-11-18 01:02:07 +0000

    Kff = 100

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -231,7 +231,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
         // Ktt = 75*10;
         // Kp = 65;
         // Kq = 14*20;
-        Kff = 1;
+        Kff = 100;
         Ktt = 1;
         Kp = 1;
         Kq = 1;
