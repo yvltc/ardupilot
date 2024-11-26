e93e076df3cd9a25375bb943f9b39984bff83921
yvltc <joaopeixoto30@gmail.com>
2024-11-26 16:16:41 +0000

    new CF

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -88,10 +88,10 @@ AP_CustomControl_INDI::AP_CustomControl_INDI(AP_CustomControl& frontend, AP_Pitc
     xSOD_u.z = 0;
 
     // command filter
-    _CF_A = 0.9048;
-    _CF_B = 0.0952;
-    _CF_C = 0.9524;
-    _CF_D = 0.04762;
+    _CF_A = 0.9802;
+    _CF_B = 0.0198;
+    _CF_C = 0.9901;
+    _CF_D = 0.0099;
 
     _CFu_A = 0.9900;
     _CFu_B = 0.0100;
