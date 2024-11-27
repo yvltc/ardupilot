b9c17ac9bf3d88cdb1ab687f0d19ab4d2f627022
yvltc <joaopeixoto30@gmail.com>
2024-11-27 17:06:28 +0000

    KVt=1

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -129,7 +129,7 @@ AP_CustomControl_INDI::AP_CustomControl_INDI(AP_CustomControl& frontend, AP_Pitc
     lambda = 0.6;
     Kff = 140;
     Ktt = 50;
-    KVt = 50;
+    KVt = 1;
     Kp = 55;
     Kq = 10;
 }
