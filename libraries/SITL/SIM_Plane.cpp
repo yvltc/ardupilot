eeca160b762686f1acfe204cb157d7d51129c199
yvltc <joaopeixoto30@gmail.com>
2024-12-02 18:03:20 +0000

    flying wing coeffs

--- libraries/SITL/SIM_Plane.cpp
+++ libraries/SITL/SIM_Plane.cpp
@@ -56,6 +56,48 @@ Plane::Plane(const char *frame_str) :
         vtail = true;
     } else if (strstr(frame_str, "-dspoilers")) {
         dspoilers = true;
+    } else if (strstr(frame_str, "-flyingwing")) {
+        float s = 0.44;
+        float b = 1.55;
+        float c = 0.2839;
+        float c_lift_0 = 0;
+        float c_lift_deltae = 0.3993;
+        float c_lift_a = 3.9934;
+        float c_lift_q = 3.1851;
+        // float mcoeff = 50;
+        // float oswald = 0.9;
+        // float alpha_stall = 0.4712;
+        // float c_drag_q = 0;
+        // float c_drag_deltae = 0.0;
+        // float c_drag_p = 0.1;
+        float c_y_0 = 0;
+        float c_y_b = -0.0025;
+        float c_y_p = 0.2620;
+        float c_y_r = -0.0673;
+        // float c_y_deltaa = 0;
+        // float c_y_deltar = -0.2;
+        float c_l_0 = 0;
+        float c_l_p = -0.4506;
+        float c_l_b = -0.1604;
+        float c_l_r = 0.3107;
+        // float c_l_deltaa = 0.25;
+        // float c_l_deltar = -0.037;
+        // float c_m_0 = 0.045;
+        float c_m_a = 0.0718;
+        float c_m_q = -2.4487;
+        float c_m_deltae = -0.0836;
+        // float c_n_0 = 0;
+        // float c_n_b = 0.25;
+        // float c_n_p = 0.022;
+        // float c_n_r = -1;
+        // float c_n_deltaa = 0.00;
+        // float c_n_deltar = 0.1;
+        // float deltaa_max = 0.3491;
+        // float deltae_max = 0.3491;
+        // float deltar_max = 0.3491;
+        // the X CoG offset should be -0.02, but that makes the plane too tail heavy
+        // in manual flight. Adjusted to -0.15 gives reasonable flight
+        Vector3f CGOffset{-0.15, 0, -0.05};
     }
     if (strstr(frame_str, "-elevrev")) {
         reverse_elevator_rudder = true;
