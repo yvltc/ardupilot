8c6ab3ac8a334080c4c2d4ea34662915dafdbd5e
yvltc <joaopeixoto30@gmail.com>
2024-12-03 19:06:45 +0000

    removed b from flying wing

--- libraries/SITL/SIM_Plane.cpp
+++ libraries/SITL/SIM_Plane.cpp
@@ -63,7 +63,7 @@ Plane::Plane(const char *frame_str) :
         // thrust_scale = (mass * GRAVITY_MSS) / hover_throttle;
 
         // coefficient.s = 0.44;
-        coefficient.b = 1.55;
+        // coefficient.b = 1.55;
         coefficient.c = 0.2839;
         // coefficient.c_lift_0 = 0;
         // coefficient.c_lift_deltae = 0.3993;
