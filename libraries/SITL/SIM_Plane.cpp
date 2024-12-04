dfc4839be9b70d2d7b0c8d02058f452713ee1810
yvltc <joaopeixoto30@gmail.com>
2024-12-04 20:02:33 +0000

    CL0 and Cm0 from CvsAOA

--- libraries/SITL/SIM_Plane.cpp
+++ libraries/SITL/SIM_Plane.cpp
@@ -65,13 +65,13 @@ Plane::Plane(const char *frame_str) :
         coefficient.s = 0.44;
         coefficient.b = 1.55;
         coefficient.c = 0.2839;
-        coefficient.c_lift_0 = 0;
+        coefficient.c_lift_0 = 0.2050;
         coefficient.c_lift_deltae = 0.3993;
         coefficient.c_lift_a = 3.9934;
         coefficient.c_lift_q = 3.1851;
         // float coefficient.mcoeff = 50;
         coefficient.oswald = 0.9;
-        // float coefficient.alpha_stall = 0.4712;
+        coefficient.alpha_stall = 0.2618;
         // float coefficient.c_drag_q = 0;
         coefficient.c_drag_deltae = 0.0054;
         // float coefficient.c_drag_p = 0.1;
@@ -87,7 +87,7 @@ Plane::Plane(const char *frame_str) :
         coefficient.c_l_r = 0.3107;
         // float coefficient.c_l_deltaa = 0.25;
         // float coefficient.c_l_deltar = -0.037;
-        // coefficient.c_m_0 = 0;       // bad!
+        coefficient.c_m_0 = 0.0017;       
         coefficient.c_m_a = 0.0718;
         coefficient.c_m_q = -2.4487;
         coefficient.c_m_deltae = -0.0836;
