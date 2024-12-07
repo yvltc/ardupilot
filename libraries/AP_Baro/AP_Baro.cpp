c9e8194ebaf43c1a9586c0fe864d88798bb032cd
yvltc <joaopeixoto30@gmail.com>
2024-12-07 22:06:13 +0000

    debugging baro

--- libraries/AP_Baro/AP_Baro.cpp
+++ libraries/AP_Baro/AP_Baro.cpp
@@ -314,7 +314,7 @@ void AP_Baro::calibrate(bool save)
         do {
             update();
             if (AP_HAL::millis() - tstart > 500) {
-                AP_BoardConfig::config_error("Baro: estou aqui");
+                //AP_BoardConfig::config_error("Baro: estou aqui");
                 AP_BoardConfig::config_error("Baro: unable to calibrate");
             }
             hal.scheduler->delay(10);
@@ -332,6 +332,7 @@ void AP_Baro::calibrate(bool save)
         do {
             update();
             if (AP_HAL::millis() - tstart > 500) {
+                AP_BoardConfig::config_error("Baro: também estou aqui");
                 AP_BoardConfig::config_error("Baro: unable to calibrate");
             }
         } while (!healthy());
