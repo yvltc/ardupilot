2f9f6fdc8a23c294ef9d140169e84c3de6e09d5a
yvltc <joaopeixoto30@gmail.com>
2024-12-09 18:59:49 +0000

    AP_BARO debugging

--- libraries/AP_Baro/AP_Baro.cpp
+++ libraries/AP_Baro/AP_Baro.cpp
@@ -929,15 +929,16 @@ void AP_Baro::update(void)
 #if HAL_BARO_WIND_COMP_ENABLED
                 corrected_pressure -= wind_pressure_correction(i);
 #endif
+                printf("Aqui\n");
                 altitude = get_altitude_difference(sensors[i].ground_pressure, corrected_pressure);
+                printf("altitude %d\n", altitude);
             } else if (sensors[i].type == BARO_TYPE_WATER) {
                 //101325Pa is sea level air pressure, 9800 Pascal/ m depth in water.
                 //No temperature or depth compensation for density of water.
                 altitude = (sensors[i].ground_pressure - corrected_pressure) / 9800.0f / _specific_gravity;
             }
             // sanity check altitude
-            sensors[i].alt_ok = !(isnan(altitude) || isinf(altitude));
-            printf("isnan %d\n", isnan(altitude));
+            sensors[i].alt_ok = !(isnan(altitude) || isinf(altitude));      // altitude é NaN
             if (sensors[i].alt_ok) {
                 sensors[i].altitude = altitude + _alt_offset_active;
             }
