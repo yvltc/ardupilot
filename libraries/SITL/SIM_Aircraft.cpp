adf101d5d276ffab2c1e226590732f653f9da2ef
yvltc <joaopeixoto30@gmail.com>
2024-12-10 18:22:13 +0000

    custom getForce debugging

--- libraries/SITL/SIM_Aircraft.cpp
+++ libraries/SITL/SIM_Aircraft.cpp
@@ -664,6 +664,7 @@ void Aircraft::update_dynamics(const Vector3f &rot_accel)
 
     // airspeed
     airspeed = velocity_air_ef.length();
+    printf("SIM_Aircraft \n");
 
     // airspeed as seen by a fwd pitot tube (limited to 120m/s)
     airspeed_pitot = constrain_float(velocity_air_bf * Vector3f(1.0f, 0.0f, 0.0f), 0.0f, 120.0f);
