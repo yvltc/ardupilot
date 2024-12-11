437930ffe7e29b10962004346eaec38a5150b480
yvltc <joaopeixoto30@gmail.com>
2024-12-11 19:05:21 +0000

    original INDI gains

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -101,24 +101,24 @@ AP_CustomControl_INDI::AP_CustomControl_INDI(AP_CustomControl& frontend, AP_Pitc
     xSOD_u.z = 0;
 
     // command filter
-    _CF_A = 0.9608;
-    _CF_B = 0.0392;
-    _CF_C = 0.9804;
-    _CF_D = 0.0196;
+    // _CF_A = 0.9608;
+    // _CF_B = 0.0392;
+    // _CF_C = 0.9804;
+    // _CF_D = 0.0196;
 
-    _CFu_A = 0.9950;
-    _CFu_B = 0.0050;
-    _CFu_C = 0.9975;
-    _CFu_D = 0.0025;
-    // _CF_A = 0;
-    // _CF_B = 1;
-    // _CF_C = 0.5;
-    // _CF_D = 0.5;
+    // _CFu_A = 0.9950;
+    // _CFu_B = 0.0050;
+    // _CFu_C = 0.9975;
+    // _CFu_D = 0.0025;
+    _CF_A = 0;
+    _CF_B = 1;
+    _CF_C = 0.5;
+    _CF_D = 0.5;
 
-    // _CFu_A = 0.8182;
-    // _CFu_B = 0.1818;
-    // _CFu_C = 0.9091;
-    // _CFu_D = 0.09091;
+    _CFu_A = 0.8182;
+    _CFu_B = 0.1818;
+    _CFu_C = 0.9091;
+    _CFu_D = 0.09091;
 
     // initial values for CF state vectors
     xCF.x = 0;
@@ -240,14 +240,14 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
         invG.a = {-397.6476,397.6476,-6.1354};
         invG.b = {-47.4484,-47.4484,-0.0598};
         invG.c = {0.4008,0.4008,6.2293};
-        // Kff = 120;
-        // Ktt = 75*10;
-        // Kp = 65;
-        // Kq = 14*20;
-        Kff = 100;
-        Ktt = 300;
-        Kp = 100;
-        Kq = 1500;
+        Kff = 120;
+        Ktt = 75;
+        Kp = 65;
+        Kq = 14;
+        // Kff = 100;
+        // Ktt = 300;
+        // Kp = 100;
+        // Kq = 1500;
         lambda = 0.3;
     }
 
