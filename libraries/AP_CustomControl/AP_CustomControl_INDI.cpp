92782cf716c8beb1feab5c48b548b69bec94c415
yvltc <joaopeixoto30@gmail.com>
2024-12-12 21:03:13 +0000

    targets

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -324,7 +324,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     // niu.y = Ktt*error[1] - Kq*q; 
     niu.z = KVt*error[2];
 
-    snprintf(buffer, sizeof(buffer), "pdot qdot Vtdot: %.4f %.4f %.4f", p_dot, q_dot, Vt_dot);
+    snprintf(buffer, sizeof(buffer), "roll pitch airspeed target: %.4f %.4f %.4f", roll_target*M_PI/180, pitch_target*M_PI/180, arspd_target);
     gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
 
     du.x = invG.a.x*lambda*(niu.x - p_dot) + invG.a.y*lambda*(niu.y - q_dot) + invG.a.z*lambda*(niu.z - Vt_dot);
