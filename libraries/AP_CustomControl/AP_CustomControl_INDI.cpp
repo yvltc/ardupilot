760c8a0d1f5c3abebce3c2622db8c48e83a310dd
yvltc <joaopeixoto30@gmail.com>
2024-12-12 23:19:22 +0000

    error

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -333,6 +333,8 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     u.x = u_0.x + du.x;
     u.y = u_0.y + du.y;
 
+    snprintf(buffer, sizeof(buffer), "error: %.4f %.4f %.4f", error.x*180/M_PI, error.y*180/M_PI, error.z*100);
+    gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
     snprintf(buffer, sizeof(buffer), "du: %.4f %.4f %.4f", du.x*180/M_PI, du.y*180/M_PI, du.z*100);
     gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
     snprintf(buffer, sizeof(buffer), "u: %.4f %.4f %.4f", u.x*180/M_PI, u.y*180/M_PI, u.z*100);
