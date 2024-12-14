b8c905d68fd14a771d07e60b423185305ad3df6a
yvltc <joaopeixoto30@gmail.com>
2024-12-14 16:59:52 +0000

    debug messages

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -337,10 +337,10 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
     snprintf(buffer, sizeof(buffer), "error: %.4f %.4f %.4f", error.x, error.y, error.z);
     gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
-    // snprintf(buffer, sizeof(buffer), "pqr: %.4f %.4f %.4f", angular_rates.x, angular_rates.y, angular_rates.z);
-    // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
-    // snprintf(buffer, sizeof(buffer), "niu: %.4f %.4f %.4f", niu.x, niu.y, niu.z);
-    // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
+    snprintf(buffer, sizeof(buffer), "pqr: %.4f %.4f %.4f", angular_rates.x, angular_rates.y, angular_rates.z);
+    gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
+    snprintf(buffer, sizeof(buffer), "niu: %.4f %.4f %.4f", niu.x, niu.y, niu.z);
+    gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
     // snprintf(buffer, sizeof(buffer), "du: %.4f %.4f %.4f", du.x*180/M_PI, du.y*180/M_PI, du.z*100);
     // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
     // snprintf(buffer, sizeof(buffer), "u: %.4f %.4f %.4f", u.x*180/M_PI, u.y*180/M_PI, u.z*100);
