1c1231461fb003ee54d58426b6078d57f91d8a8e
yvltc <joaopeixoto30@gmail.com>
2024-12-15 23:23:07 +0000

    s b c CL

--- libraries/SITL/SIM_Plane.cpp
+++ libraries/SITL/SIM_Plane.cpp
@@ -65,12 +65,12 @@ Plane::Plane(const char *frame_str) :
         mass = 1.2650;
         thrust_scale = (mass * GRAVITY_MSS) / hover_throttle;
 
-        // coefficient.s = 0.44;
-        // coefficient.b = 1.55;
-        // coefficient.c = 0.2839;
+        coefficient.s = 0.44;
+        coefficient.b = 1.55;
+        coefficient.c = 0.2839;
         // coefficient.c_lift_0 = 0.2050;
-        // coefficient.c_lift_deltae = 0.3993;
-        // coefficient.c_lift_a = 3.9934;
+        coefficient.c_lift_deltae = 0.3993;
+        coefficient.c_lift_a = 3.9934;
         // coefficient.c_lift_q = 3.1851;
         // // float coefficient.mcoeff = 50;
         // coefficient.oswald = 0.9;
