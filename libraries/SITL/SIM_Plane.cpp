494146aa6b227dfffc2c3de67e83cba2503e2b60
yvltc <joaopeixoto30@gmail.com>
2024-12-16 00:02:44 +0000

    Cmde

--- libraries/SITL/SIM_Plane.cpp
+++ libraries/SITL/SIM_Plane.cpp
@@ -93,8 +93,8 @@ Plane::Plane(const char *frame_str) :
         // Cm causa problemas!
         // coefficient.c_m_0 = -0.0148;       
         // coefficient.c_m_a = 0.0718;
-        coefficient.c_m_q = -2.4487;
-        // coefficient.c_m_deltae = -0.0836;
+        // coefficient.c_m_q = -2.4487;
+        coefficient.c_m_deltae = -0.0836;
         // coefficient.c_m_adot = -0.4897;
         // Cn também não é particularmente bom com os outros mas aguenta-se sozinho
         // coefficient.c_n_0 = 0;
