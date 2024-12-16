3b6f270bddcb0b50f9069e14e52fbebad276d81a
yvltc <joaopeixoto30@gmail.com>
2024-12-16 00:00:16 +0000

    Cmq

--- libraries/SITL/SIM_Plane.cpp
+++ libraries/SITL/SIM_Plane.cpp
@@ -92,8 +92,8 @@ Plane::Plane(const char *frame_str) :
         // // float coefficient.c_l_deltar = -0.037;
         // Cm causa problemas!
         // coefficient.c_m_0 = -0.0148;       
-        coefficient.c_m_a = 0.0718;
-        // coefficient.c_m_q = -2.4487;
+        // coefficient.c_m_a = 0.0718;
+        coefficient.c_m_q = -2.4487;
         // coefficient.c_m_deltae = -0.0836;
         // coefficient.c_m_adot = -0.4897;
         // Cn também não é particularmente bom com os outros mas aguenta-se sozinho
