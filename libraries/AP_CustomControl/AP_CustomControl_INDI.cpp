ddf859d31c372ec25abb5119a94d518e727c4058
yvltc <joaopeixoto30@gmail.com>
2025-01-09 21:25:39 +0000

    lambda 0.1

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -259,7 +259,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
         // Ktt = 300;
         // Kp = 100;
         // Kq = 1500;
-        lambda = 0.6;
+        lambda = 0.1;
     }
 
     invert_G = invG.invert();
