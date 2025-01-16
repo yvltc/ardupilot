685620eb088d2659ddf3ba2af85c37d519644de3
yvltc <joaopeixoto30@gmail.com>
2025-01-16 17:48:17 +0000

    debugging - only xCF

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -297,14 +297,14 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     aux.z = 0;
 
     //debug
-    roll_target = 0;
-    pitch_target = -0.0015*18000/M_PI;
-    arspd_target = 20;
-    phi = -0.0108;
-    theta = -0.0189;
-    Vt = 19.9519;
-    angular_rates = {-0.3286, 0.2890, 19.9519};
-    u_0 = {0.0783, -0.2300, 0.0572};
+    // roll_target = 0;
+    // pitch_target = -0.0015*18000/M_PI;
+    // arspd_target = 20;
+    // phi = -0.0108;
+    // theta = -0.0189;
+    // Vt = 19.9519;
+    // angular_rates = {-0.3286, 0.2890, 19.9519};
+    // u_0 = {0.0783, -0.2300, 0.0572};
     xCF = {0.0767, -0.2354, 0.0594};
     char buffer[80];  // Create a buffer to hold the formatted message
     // snprintf(buffer, sizeof(buffer), "xSODp: %.6f %.6f %.6f", xSOD_p.x, xSOD_p.y, xSOD_p.z);
