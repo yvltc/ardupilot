d82692f43fa2e3a903bc3a74ea31a6ea0337317d
yvltc <joaopeixoto30@gmail.com>
2025-01-18 18:38:52 +0000

    dtmin=0.1

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -58,7 +58,7 @@ AP_CustomControl_INDI::AP_CustomControl_INDI(AP_CustomControl& frontend, AP_Pitc
     // saturation limits
     ddmax = 30*M_PI/180;
     dtmax = 1;
-    dtmin = 0;
+    dtmin = 0.1;
 
     // SOD - each vector is a row
     // SOD_A.a = {0.846153846153846,0.003076923076923,0};
@@ -240,8 +240,8 @@ float AP_CustomControl_INDI::get_Vt_out(void)
 
     // return what ArduPlane main controller outputted
     // return SRV_Channels::get_output_scaled(SRV_Channel::k_throttle);
-    // return u_0[2]*100;
-    return u_0[2];
+    return u_0[2]*100;
+    // return u_0[2];
 }
 
 void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
@@ -453,8 +453,8 @@ void AP_CustomControl_INDI::reset(void)
 
     u_0.x = SRV_Channels::get_output_scaled(SRV_Channel::k_aileron)*M_PI/18000;
     u_0.y = SRV_Channels::get_output_scaled(SRV_Channel::k_elevator)*M_PI/18000;
-    // u_0.z = SRV_Channels::get_output_scaled(SRV_Channel::k_throttle)/100;
-    u_0.z = SRV_Channels::get_output_scaled(SRV_Channel::k_throttle);
+    u_0.z = SRV_Channels::get_output_scaled(SRV_Channel::k_throttle)/100;
+    // u_0.z = SRV_Channels::get_output_scaled(SRV_Channel::k_throttle);
     // printf("%f\n", u_0.z*100);
 }
 
