137fc8e25bb5d0845925838463f7b50781467081
yvltc <joaopeixoto30@gmail.com>
2025-01-20 16:11:58 +0000

    add message again

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -194,9 +194,9 @@ float AP_CustomControl_INDI::get_roll_out(float roll_target)
     // gcs().send_text(MAV_SEVERITY_INFO, "Timestamp: %d miliseconds", timestamp);
 
     //gcs().send_text(MAV_SEVERITY_INFO, "roll INDI custom controller working");
-    // char buffer[80];  // Create a buffer to hold the formatted message
-    // snprintf(buffer, sizeof(buffer), "roll INDI custom controller working, u_0[0] u_0[1] u_0[2]: %.2f %.2f %.2f", u_0[0]*180/M_PI,  u_0[1]*180/M_PI, u_0[2]*100);
-    // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
+    char buffer[80];  // Create a buffer to hold the formatted message
+    snprintf(buffer, sizeof(buffer), "roll INDI custom controller working, u_0[0] u_0[1] u_0[2]: %.2f %.2f %.2f", u_0[0]*180/M_PI,  u_0[1]*180/M_PI, u_0[2]*100);
+    gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
 
     // return what ArduPlane main controller outputted
     //return SRV_Channels::get_output_scaled(SRV_Channel::k_aileron);
@@ -212,9 +212,9 @@ float AP_CustomControl_INDI::get_pitch_out(float pitch_target)
     // we don't need to do anything else
 
     //gcs().send_text(MAV_SEVERITY_INFO, "pitch INDI custom controller working");
-    char buffer[80];  // Create a buffer to hold the formatted message
-    snprintf(buffer, sizeof(buffer), "pitch INDI custom controller working, u_0[1]: %.2f", u_0[1]*18000/M_PI);
-    gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
+    // char buffer[80];  // Create a buffer to hold the formatted message
+    // snprintf(buffer, sizeof(buffer), "pitch INDI custom controller working, u_0[1]: %.2f", u_0[1]*18000/M_PI);
+    // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
 
     // return what ArduPlane main controller outputted
     //return SRV_Channels::get_output_scaled(SRV_Channel::k_elevator);
