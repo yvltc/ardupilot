c57c78248b0373aff2930d4d3a5e197fbc847713
yvltc <joaopeixoto30@gmail.com>
2025-01-20 00:45:49 +0000

    -Ki

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -432,7 +432,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
 
     // PID
     Vector3f K_p = {1,-0.11,1};
-    Vector3f K_d = {0.04,0.3,0};
+    Vector3f K_d = {-0.04,-0.3,0};
     Vector3f K_i = {0,0,0};
 
     Vector3f u_Kp;
