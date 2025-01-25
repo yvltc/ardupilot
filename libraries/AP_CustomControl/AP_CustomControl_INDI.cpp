045c9a859d6bbcf1110c3c12c291ed1fb9c1a8cf
yvltc <joaopeixoto30@gmail.com>
2025-01-25 21:18:56 +0000

    PID with CF

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -413,7 +413,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     // snprintf(buffer, sizeof(buffer), "xCF: %.6f %.6f %.6f", xCF.x, xCF.y, xCF.z);
     // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
     // command filter
-    sspace(u, xCF, CF_A, CF_B, CF_C, CF_D, &u, &xCF);
+    // sspace(u, xCF, CF_A, CF_B, CF_C, CF_D, &u, &xCF);
 
     // snprintf(buffer, sizeof(buffer), "uCF: %.6f %.6f %.6f", u.x, u.y, u.z);
     // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
@@ -475,6 +475,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     u = u_Kp + u_Kd + u_Ki; //+ u_Kff;
     saturate(-0.9*ddmax, 0.9*ddmax, &u.x);
     saturate(-0.9*ddmax, 0.9*ddmax, &u.y);
+    sspace(u, xCF, CF_A, CF_B, CF_C, CF_D, &u, &xCF);
     // printf("%f\n", roll_target*M_PI/18000);
     u_0 = u;
 }
