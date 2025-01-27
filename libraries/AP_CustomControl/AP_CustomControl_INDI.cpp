7892737738ae4b9ecf2919d06dac204e1bbd4435
yvltc <joaopeixoto30@gmail.com>
2025-01-27 22:00:20 +0000

    debugging

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -306,16 +306,16 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     aux.z = 0;
 
     //debug
-    // roll_target = 0;
-    // pitch_target = -0.0015*18000/M_PI;
-    // arspd_target = 20;
-    // phi = -0.0108;
-    // theta = -0.0189;
-    // Vt = 19.9519;
-    // angular_rates = {-0.3286, 0.2890, 19.9519};
-    // u_0 = {0.0783, -0.2300, 0.0572};
-    // xCF = {0.0767, -0.2354, 0.0594};
-    // char buffer[80];  // Create a buffer to hold the formatted message
+    roll_target = 0;
+    pitch_target = -0.0015*18000/M_PI;
+    arspd_target = 20;
+    phi = -0.0108;
+    theta = -0.0189;
+    Vt = 19.9519;
+    angular_rates = {-0.3286, -0.2890, 19.9519};
+    u_0 = {0.0783, -0.2301, 0.0572};
+    xCF = {0.0768, -0.2354, 0.0594};
+    char buffer[80];  // Create a buffer to hold the formatted message
     // snprintf(buffer, sizeof(buffer), "xSODp: %.6f %.6f %.6f", xSOD_p.x, xSOD_p.y, xSOD_p.z);
     // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
     // snprintf(buffer, sizeof(buffer), "xSODq: %.6f %.6f %.6f", xSOD_q.x, xSOD_q.y, xSOD_q.z);
@@ -415,8 +415,8 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     // command filter
     sspace(u, xCF, CF_A, CF_B, CF_C, CF_D, &u, &xCF);
 
-    // snprintf(buffer, sizeof(buffer), "uCF: %.6f %.6f %.6f", u.x, u.y, u.z);
-    // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
+    snprintf(buffer, sizeof(buffer), "uCF: %.6f %.6f %.6f", u.x, u.y, u.z);
+    gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
     // snprintf(buffer, sizeof(buffer), "x_next_CF: %.6f %.6f %.6f", xCF.x, xCF.y, xCF.z);
     // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
     
@@ -477,7 +477,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     saturate(-0.9*ddmax, 0.9*ddmax, &u.y);
     // sspace(u, xCF, CF_A, CF_B, CF_C, CF_D, &u, &xCF);
     // printf("%f\n", roll_target*M_PI/18000);
-    u_0 = u;
+    // u_0 = u;
 }
 
 // reset controller to avoid build up on the ground
