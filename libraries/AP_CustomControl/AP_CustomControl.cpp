2456ee9acde3def8de3a88d46b23a1e22575c5d1
yvltc <joaopeixoto30@gmail.com>
2025-01-28 14:20:49 +0000

    CF in PID

--- libraries/AP_CustomControl/AP_CustomControl.cpp
+++ libraries/AP_CustomControl/AP_CustomControl.cpp
@@ -93,7 +93,7 @@ void AP_CustomControl::update(float roll_target, float pitch_target)
         // roll_target = 0;
         // pitch_target = 0;
         // update tem de receber roll_target pitch_target e TAS_dem
-        printf("%f %f\n", roll_target, pitch_target);
+        // printf("%f %f\n", roll_target, pitch_target);
         _backend->update(roll_target, pitch_target);
         roll_out = _backend->get_roll_out(roll_target);
         pitch_out = _backend->get_pitch_out(pitch_target);
