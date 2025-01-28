0b92b13325b25ba5c3b47c28422f334308f1d78e
yvltc <joaopeixoto30@gmail.com>
2025-01-28 23:42:22 +0000

    debugging

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -466,8 +466,8 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     // u_Kff.z = K_ff.z*arspd_target;
 
     u = u_Kp + u_Kd + u_Ki; //+ u_Kff;
-    // saturate(-0.9*ddmax, 0.9*ddmax, &u.x);
-    // saturate(-0.9*ddmax, 0.9*ddmax, &u.y);
+    saturate(-0.9*ddmax, 0.9*ddmax, &u.x);
+    saturate(-0.9*ddmax, 0.9*ddmax, &u.y);
     // sspace(u, xCF, CF_A, CF_B, CF_C, CF_D, &u, &xCF);
     // printf("%f\n", roll_target*M_PI/18000);
     u_0 = u;
