5851d34041b3bf7de1d7bc080a48b24bfbe0025b
yvltc <joaopeixoto30@gmail.com>
2025-01-28 18:01:59 +0000

    debugging

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -424,9 +424,9 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
 
     // PID
-    Vector3f K_p = {1,-0.11,100};
-    Vector3f K_d = {-0.04,-0.3,10};
-    Vector3f K_i = {70,0,0};
+    Vector3f K_p = {1,-0.11,1};
+    Vector3f K_d = {-0.04,-0.3,0};
+    Vector3f K_i = {70,1000,0};
     // Vector3f K_p = {0.08,0.04,0.01};
     // Vector3f K_d = {0,0,0};
     // Vector3f K_i = {0.15,0.15,0};
