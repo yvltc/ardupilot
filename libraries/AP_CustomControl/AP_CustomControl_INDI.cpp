9821acf23e3da8269e09ab7021d0f72294632266
yvltc <joaopeixoto30@gmail.com>
2025-01-28 00:56:10 +0000

    debugging

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -399,14 +399,14 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
         // saturate(-0.9*ddmax, 0.9*ddmax, &u.y);
         saturate(dtmin, dtmax, &u.z);
 
-        snprintf(buffer, sizeof(buffer), "usat: %.6f %.6f %.6f", u.x, u.y, u.z);
-        gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
+        // snprintf(buffer, sizeof(buffer), "usat: %.6f %.6f %.6f", u.x, u.y, u.z);
+        // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
     }
 
     // snprintf(buffer, sizeof(buffer), "xCF: %.6f %.6f %.6f", xCF.x, xCF.y, xCF.z);
     // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
     // command filter
-    sspace(u, xCF, CF_A, CF_B, CF_C, CF_D, &u, &xCF);
+    // sspace(u, xCF, CF_A, CF_B, CF_C, CF_D, &u, &xCF);
 
     snprintf(buffer, sizeof(buffer), "uCF: %.6f %.6f %.6f", u.x, u.y, u.z);
     gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
