6ff0ff914aa1ab8b89fb16f752ea89aa508ee19e
yvltc <joaopeixoto30@gmail.com>
2025-02-01 16:33:22 +0000

    compare custom and default controller outputs

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -420,7 +420,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     u_ardupilot.x = SRV_Channels::get_output_scaled(SRV_Channel::k_aileron);
     u_ardupilot.y = SRV_Channels::get_output_scaled(SRV_Channel::k_elevator);
     u_ardupilot.z = SRV_Channels::get_output_scaled(SRV_Channel::k_throttle);
-    snprintf(buffer, sizeof(buffer), "ArduPilot controller output, u_0[0] u_0[1] u_0[2]: %.2f %.2f %.2f", u_ardupilot.x,  u_ardupilot.y, u_ardupilot.z);
+    snprintf(buffer, sizeof(buffer), "ArduPilot controller output, u_0[0] u_0[1] u_0[2]: %.2f %.2f %.2f", u_ardupilot.x/100,  u_ardupilot.y/100, u_ardupilot.z);
     gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
 
     u_0.x = u.x;
