0748faae21e811beab1bf1c577efb8c7dfa7c6b6
yvltc <joaopeixoto30@gmail.com>
2025-02-01 16:58:18 +0000

    compare custom and default controller outputs

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -207,8 +207,8 @@ float AP_CustomControl_INDI::get_pitch_out(float pitch_target)
 
     // return what ArduPlane main controller outputted
     //return SRV_Channels::get_output_scaled(SRV_Channel::k_elevator);
-    // return -u_0[1]*18000/M_PI;
-    return u_0[1]*18000/M_PI;
+    return -u_0[1]*18000/M_PI;
+    // return u_0[1]*18000/M_PI;
 }
 
 //return yaw controller output
@@ -475,6 +475,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     u = u_Kp + u_Kd + u_Ki; //+ u_Kff;
     saturate(-0.9*ddmax, 0.9*ddmax, &u.x);
     saturate(-0.9*ddmax, 0.9*ddmax, &u.y);
+    saturate(dtmax, dtmin, &u.z);
     // sspace(u, xCF, CF_A, CF_B, CF_C, CF_D, &u, &xCF);
     // printf("%f\n", roll_target*M_PI/18000);
     u_0 = u;
