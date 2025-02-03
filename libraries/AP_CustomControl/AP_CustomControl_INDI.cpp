17013e0fb0d399861075c1ce20f4fffc6321b3c7
yvltc <joaopeixoto30@gmail.com>
2025-02-03 21:10:51 +0000

    error_0

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -438,7 +438,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
         char buffer[80];
         Vector3f K_p = {1,-0.11,1};
         Vector3f K_d = {-0.04,-0.3,0};
-        Vector3f K_i = {70,0,0};
+        Vector3f K_i = {0,0,0};
         // Vector3f K_p = {0.08,0.04,0.01};
         // Vector3f K_d = {0,0,0};
         // Vector3f K_i = {0.15,0.15,0};
