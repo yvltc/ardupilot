688e9ea83ab149c39a4c9250fe25736927154297
yvltc <joaopeixoto30@gmail.com>
2025-02-04 18:22:50 +0000

    0.95ddmax saturation

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -377,8 +377,8 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     // snprintf(buffer, sizeof(buffer), "u: %.6f %.6f %.6f", u.x*180/M_PI, u.y*180/M_PI, u.z*100);
     // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
 
-    saturate(-0.9*ddmax, 0.9*ddmax, &u.x);
-    saturate(-0.9*ddmax, 0.9*ddmax, &u.y);
+    saturate(-0.95*ddmax, 0.95*ddmax, &u.x);
+    saturate(-0.95*ddmax, 0.95*ddmax, &u.y);
 
     if (is_gliding)
     {
@@ -408,7 +408,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     // snprintf(buffer, sizeof(buffer), "xCF: %.6f %.6f %.6f", xCF.x, xCF.y, xCF.z);
     // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
     // command filter
-    // sspace(u, xCF, CF_A, CF_B, CF_C, CF_D, &u, &xCF);
+    sspace(u, xCF, CF_A, CF_B, CF_C, CF_D, &u, &xCF);
 
     // snprintf(buffer, sizeof(buffer), "uCF: %.6f %.6f %.6f", u.x, u.y, u.z);
     // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
