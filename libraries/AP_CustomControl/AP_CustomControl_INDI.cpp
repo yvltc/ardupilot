309247b873ab40e359ee6288be4547fcdef12a24
yvltc <joaopeixoto30@gmail.com>
2025-02-04 17:43:21 +0000

    0.95ddmax saturation

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -483,8 +483,8 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
         // u_Kff.z = K_ff.z*arspd_target;
 
         u = u_Kp + u_Kd + u_Ki; //+ u_Kff;
-        saturate(-0.9*ddmax, 0.9*ddmax, &u.x);
-        saturate(-0.9*ddmax, 0.9*ddmax, &u.y);
+        saturate(-0.95*ddmax, 0.95*ddmax, &u.x);
+        saturate(-0.95*ddmax, 0.95*ddmax, &u.y);
         saturate(dtmin, dtmax, &u.z);
         sspace(u, xCF, CF_A, CF_B, CF_C, CF_D, &u, &xCF);
         // printf("%f\n", roll_target*M_PI/18000);
