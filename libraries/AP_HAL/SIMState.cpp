97d746dad4b0347a7dd1e4504cd911af4b4b472e
yvltc <joaopeixoto30@gmail.com>
2025-02-05 19:32:09 +0000

    print in SIMState

--- libraries/AP_HAL/SIMState.cpp
+++ libraries/AP_HAL/SIMState.cpp
@@ -8,6 +8,8 @@
  *  there into here.
  */
 
+#include <iostream>
+
 #include <SITL/SIM_Multicopter.h>
 #include <SITL/SIM_Helicopter.h>
 #include <SITL/SIM_SingleCopter.h>
@@ -73,6 +75,7 @@ void SIMState::update()
     if (!init_done) {
         init_done = true;
         sitl_model = SITL::AP_SIM_FRAME_CLASS::create(AP_SIM_FRAME_STRING);
+        printf("Boas\n");
     }
 
     _fdm_input_step();
