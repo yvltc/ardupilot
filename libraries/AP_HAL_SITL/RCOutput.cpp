112eb3dbba2bac9726045deb93493de81bebcbaa
yvltc <joaopeixoto30@gmail.com>
2025-02-09 01:09:11 +0000

    debug

--- libraries/AP_HAL_SITL/RCOutput.cpp
+++ libraries/AP_HAL_SITL/RCOutput.cpp
@@ -59,7 +59,7 @@ void RCOutput::write(uint8_t ch, uint16_t period_us)
             period_us = 0;
         }
     }
-
+    printf("Boassssssssss\n");
     _sitlState->output_ready = true;
     // FIXME: something in sitl is expecting to be able to read and write disabled channels
     if (ch < SITL_NUM_CHANNELS /*&& (_enable_mask & (1U<<ch))*/) {
@@ -67,7 +67,6 @@ void RCOutput::write(uint8_t ch, uint16_t period_us)
             _pending[ch] = period_us;
         } else {
             _sitlState->pwm_output[ch] = period_us;
-            printf("%d %d\n", _sitlState->pwm_output[ch], ch);
         }
     }
 }
