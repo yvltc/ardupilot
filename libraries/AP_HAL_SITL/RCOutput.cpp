bff386c2dd5ada0bede23b6ba6d03e300c546d4c
yvltc <joaopeixoto30@gmail.com>
2025-02-09 19:27:58 +0000

    debug

--- libraries/AP_HAL_SITL/RCOutput.cpp
+++ libraries/AP_HAL_SITL/RCOutput.cpp
@@ -65,7 +65,7 @@ void RCOutput::write(uint8_t ch, uint16_t period_us)
     if (ch < SITL_NUM_CHANNELS /*&& (_enable_mask & (1U<<ch))*/) {
         if (_corked) {
             _pending[ch] = period_us;
-            printf("%d\n", period_us);
+            // printf("%d\n", period_us);
         } else {
             _sitlState->pwm_output[ch] = period_us;
         }
