99b8e71833dd4820fc58ba9af7473da8295ce645
yvltc <joaopeixoto30@gmail.com>
2025-02-09 00:14:08 +0000

    debug

--- libraries/AP_HAL_SITL/SITL_State.cpp
+++ libraries/AP_HAL_SITL/SITL_State.cpp
@@ -471,7 +471,7 @@ void SITL_State::_simulator_servos(struct sitl_input &input)
             input.servos[i] = 0;
         } else {
             input.servos[i] = pwm_output[i];
-            if (AP_HAL::micros() < 1500)
+            if (AP_HAL::micros() > 1000000 && AP_HAL::micros() < 1001000)
                 printf("%d %d\n", input.servos[i], i);
         }
     }
