4a1d7ace91000f1f862f915e98fe847d2d93fd1d
yvltc <joaopeixoto30@gmail.com>
2025-02-11 23:13:10 +0000

    throttle/2

--- libraries/SITL/SIM_Plane.cpp
+++ libraries/SITL/SIM_Plane.cpp
@@ -494,7 +494,7 @@ void Plane::calculate_forces(const struct sitl_input &input, Vector3f &rot_accel
         throttle = filtered_servo_range(input, 2);
     }
     
-    float thrust     = throttle/4;
+    float thrust     = throttle/2;
 
     battery_voltage = sitl->batt_voltage - 0.7*throttle;
     battery_current = (battery_voltage/sitl->batt_voltage)*50.0f*sq(throttle);
