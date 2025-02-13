78ece3b5994ecde8c58e4d9a3d6b0f5572a3a52a
yvltc <joaopeixoto30@gmail.com>
2025-02-13 11:14:24 +0000

    debug

--- libraries/AP_Motors/AP_MotorsMatrix.cpp
+++ libraries/AP_Motors/AP_MotorsMatrix.cpp
@@ -17,6 +17,8 @@
 #include "AP_MotorsMatrix.h"
 #include <AP_Vehicle/AP_Vehicle_Type.h>
 
+#include <iostream>
+
 extern const AP_HAL::HAL& hal;
 
 // init
@@ -36,6 +38,8 @@ void AP_MotorsMatrix::init(motor_frame_class frame_class, motor_frame_type frame
 
     // enable fast channels or instant pwm
     set_update_rate(_speed_hz);
+
+    printf("boas\n");
 }
 
 #if AP_SCRIPTING_ENABLED
--- libraries/AP_Motors/AP_MotorsSingle.cpp
+++ libraries/AP_Motors/AP_MotorsSingle.cpp
@@ -19,8 +19,6 @@
 #include <GCS_MAVLink/GCS.h>
 #include <SRV_Channel/SRV_Channel.h>
 
-#include <iostream>
-
 extern const AP_HAL::HAL& hal;
 
 // init
@@ -69,7 +67,6 @@ void AP_MotorsSingle::output_to_motors()
     if (!initialised_ok()) {
         return;
     }
-    printf("BOAS\n");
     switch (_spool_state) {
         case SpoolState::SHUT_DOWN:
             // sends minimum values out to the motors
