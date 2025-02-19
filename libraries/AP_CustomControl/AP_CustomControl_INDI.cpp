9560b5fc31f23464c5e58f7f42b56dd7e7aa9a61
yvltc <joaopeixoto30@gmail.com>
2025-02-19 18:40:12 +0000

    changed PID gains for higher drag

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -437,7 +437,8 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     {
         ddmax = 45*M_PI/180;
         // char buffer[80];
-        Vector3f K_p = {1,-0.11,10};
+        // com drag certo deve ser 1 no K_p da velocidade
+        Vector3f K_p = {1,-0.11,100};
         Vector3f K_d = {-0.04,-0.3,0};
         Vector3f K_i = {-70,-150,0};
         // Vector3f K_p = {0.08,0.04,0.01};
