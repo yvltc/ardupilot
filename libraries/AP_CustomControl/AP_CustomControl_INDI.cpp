5bf719f087ec14d4089a2b831e113a2647d58356
yvltc <joaopeixoto30@gmail.com>
2025-02-24 00:02:45 +0000

    PID gains

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -440,7 +440,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
         // com drag certo deve ser 1 no K_p da velocidade e -150 no K_i de pitch
         Vector3f K_p = {1,-0.11,10};
         Vector3f K_d = {-0.04,-0.3,0};
-        Vector3f K_i = {-70,-1,0};
+        Vector3f K_i = {-70,-50,0};
         // Vector3f K_p = {0.08,0.04,0.01};
         // Vector3f K_d = {0,0,0};
         // Vector3f K_i = {0.15,0.15,0};
