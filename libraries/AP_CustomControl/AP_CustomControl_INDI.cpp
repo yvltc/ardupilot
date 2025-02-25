4fed0b8066aa1e8cb760b442e205a61368fa943f
yvltc <joaopeixoto30@gmail.com>
2025-02-25 00:10:16 +0000

    tuning PID gains

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -442,7 +442,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
         // Vector3f K_d = {-0.04,-0.3,0};
         // Vector3f K_i = {-70,-150,0};
         Vector3f K_p = {1,-0.11,10};
-        Vector3f K_d = {-0.04,-0.3,-10};
+        Vector3f K_d = {-0.04,-0.3,0};
         Vector3f K_i = {-70,-150,-10};
         // Vector3f K_ff = {0.345, 0.345, 0};
 
