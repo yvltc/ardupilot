e087a8ab4b86e52e0b4ff8ec1b738e9f548a08a0
yvltc <joaopeixoto30@gmail.com>
2025-03-06 00:45:31 +0000

    sat 45 for INDI

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -58,7 +58,7 @@ AP_CustomControl_INDI::AP_CustomControl_INDI(AP_CustomControl& frontend, AP_Pitc
     // u_0.z = 0.6428;
 
     // saturation limits
-    ddmax = 30*M_PI/180;
+    ddmax = 45*M_PI/180;
     dtmax = 1;
     dtmin = 0;
 
