6ebcff129d65f4a37bbfeda515f4a16b329c612a
yvltc <joaopeixoto30@gmail.com>
2025-03-13 23:05:32 +0000

    mixer debug

--- libraries/SRV_Channel/SRV_Channel_aux.cpp
+++ libraries/SRV_Channel/SRV_Channel_aux.cpp
@@ -339,7 +339,7 @@ void SRV_Channels::enable_by_mask(uint32_t mask)
  */
 void SRV_Channels::set_output_pwm(SRV_Channel::Aux_servo_function_t function, uint16_t value)
 {
-    printf("boas\n")
+    printf("boas\n");
     if (!function_assigned(function)) {
         return;
     }
