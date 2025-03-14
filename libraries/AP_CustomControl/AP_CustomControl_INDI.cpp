248ace144c5b730e8c3759b7e9494d26c294fdf3
yvltc <joaopeixoto30@gmail.com>
2025-03-14 17:05:42 +0000

    separated lambda into each input

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -62,7 +62,7 @@ AP_CustomControl_INDI::AP_CustomControl_INDI(AP_CustomControl& frontend, AP_Pitc
     // u_0.z = 0.6428;
 
     // saturation limits
-    ddmax = 30*M_PI/180;
+    ddmax = 50*M_PI/180;
     dtmax = 1;
     dtmin = 0;
 
