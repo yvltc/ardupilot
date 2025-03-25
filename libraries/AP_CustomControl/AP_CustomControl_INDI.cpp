e7fc7ddfa4e2adc648d805bb79e7891b1e7e886f
yvltc <joaopeixoto30@gmail.com>
2025-03-25 23:31:46 +0000

    changed CFu

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -126,15 +126,15 @@ AP_CustomControl_INDI::AP_CustomControl_INDI(AP_CustomControl& frontend, AP_Pitc
     _CF_C = 0.2857;
     _CF_D = 0.7143;
 
-    // _CFu_A = 0.9048;
-    // _CFu_B = 0.0952;
-    // _CFu_C = 0.9524;
-    // _CFu_D = 0.0476;
+    _CFu_A = 0.9048;
+    _CFu_B = 0.0952;
+    _CFu_C = 0.9524;
+    _CFu_D = 0.0476;
 
-    _CFu_A = 0.8182;
-    _CFu_B = 0.1818;
-    _CFu_C = 0.9091;
-    _CFu_D = 0.09091;
+    // _CFu_A = 0.8182;
+    // _CFu_B = 0.1818;
+    // _CFu_C = 0.9091;
+    // _CFu_D = 0.09091;
 
     // _CFu_A = 0.3333;
     // _CFu_B = 0.6667;
