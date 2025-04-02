0e71c37196d6569087ac275c4407cfb02e7b9e96
yvltc <joaopeixoto30@gmail.com>
2025-04-02 17:29:06 +0100

    PID debug

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -468,7 +468,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
         // Vector3f K_d = {-0.04,-0.3,0};
         // Vector3f K_i = {-70,-150,0};
         Vector3f K_p = {1,-0.11,1};
-        Vector3f K_d = {-0.04,-0.3,10};
+        Vector3f K_d = {-0.04,-0.3,100};
         Vector3f K_i = {-70,-150,0};
 
         // {p, i, d, ...}
