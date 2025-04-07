2bf9f0ddacc95d67277c66ce7aee9835415e68ab
yvltc <joaopeixoto30@gmail.com>
2025-04-07 19:50:38 +0100

    INDI logger message

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -536,11 +536,12 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     //                             du.x*180/M_PI,
     //                             du.y*180/M_PI,
     //                             du.z);
-    AP::logger().Write("INDI", "TimeUS,RollTarget,PitchTarget,AirspeedTarget", "Qfff",
+    AP::logger().Write("INDI", "TimeUS,RollTarget,PitchTarget,AirspeedTarget,DeltaAil", "Qffff",
                                 AP_HAL::micros64(),
                                 roll_target,
                                 pitch_target,
-                                arspd_target);
+                                arspd_target,
+                                du.x*180/M_PI);
 }
 
 // reset controller to avoid build up on the ground
