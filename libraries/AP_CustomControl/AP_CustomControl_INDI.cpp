a40039ec2edd414b596beb9b50c1335afe49cc27
yvltc <joaopeixoto30@gmail.com>
2025-04-07 01:14:31 +0100

    roll reference

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -319,7 +319,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     aux.z = 0;
 
     //debug
-    // roll_target = 0;
+    roll_target = 3000;
     // pitch_target = -0.0015*18000/M_PI;
     // arspd_target = 20;
     // phi = -0.0087;
@@ -469,7 +469,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
         // Vector3f K_i = {-70,-150,0};
         Vector3f K_p = {1,-0.11,1};
         Vector3f K_d = {-0.04,-0.3,0};
-        Vector3f K_i = {-70,-150,100000000};
+        Vector3f K_i = {-70,-150,0};
 
         // {p, i, d, ...}
         // AC_PID rate_pid{0.08, 0.15, 0, 0.345, 0.666, 3, 0, 12, 150, 1};      // Roll
