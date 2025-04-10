26c965116ba995328dbd52909bf38c008af4cee8
yvltc <joaopeixoto30@gmail.com>
2025-04-10 17:52:35 +0100

    debugging

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -411,8 +411,8 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     // u_0.y = SRV_Channels::get_output_scaled(SRV_Channel::k_elevator)*M_PI/18000;
     // u_0.z = SRV_Channels::get_output_scaled(SRV_Channel::k_throttle)/100;
 
-    u.x = -u_0.x - du.x;
-    u.y = -u_0.y - du.y;
+    u.x = -u_0.x + du.x;
+    u.y = -u_0.y + du.y;
     // u.x = du.x;
     // u.y = du.y;
 
