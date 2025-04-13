4ec296a5627f926b2fb48b1b244c6d48596d7eae
yvltc <joaopeixoto30@gmail.com>
2025-04-14 00:31:12 +0100

    debug

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -329,6 +329,8 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
         use_TAS = _ahrs.airspeed_estimate(Vt);
     }
 
+    Vt = _tecs.get_TAS_demand();
+
     // AHRS airspeed estimate true (true airspeed)
     // altitude desejada no TECS se quiser
 
