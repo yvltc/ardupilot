af68103c314f8e50d02ebd39a0bf92405adcdf52
yvltc <joaopeixoto30@gmail.com>
2025-04-13 21:12:50 +0100

    debug

--- libraries/AP_TECS/AP_TECS.cpp
+++ libraries/AP_TECS/AP_TECS.cpp
@@ -810,8 +810,8 @@ void AP_TECS::_update_throttle_with_airspeed(void)
         if (test)
         {
             float K_p = 10;
-            float K_d = 1000;
-            float K_i = 0;
+            float K_d = 100;
+            float K_i = 1;
 
             float u_Kp;
             float u_Kd;
