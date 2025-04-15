6aab3a89825bf4dc294e41bca0c593927595a3a7
yvltc <joaopeixoto30@gmail.com>
2025-04-16 00:43:59 +0100

    debug

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -144,16 +144,16 @@ AP_CustomControl_INDI::AP_CustomControl_INDI(AP_CustomControl& frontend, AP_Pitc
     // _CF_D = 0.7143;
 
     // default
-    // _CFu_A = 0.9048;
-    // _CFu_B = 0.0952;
-    // _CFu_C = 0.9524;
-    // _CFu_D = 0.0476;
+    _CFu_A = 0.9048;
+    _CFu_B = 0.0952;
+    _CFu_C = 0.9524;
+    _CFu_D = 0.0476;
 
     // tau = 2s
-    _CFu_A = 0.9950;
-    _CFu_B = 0.0050;
-    _CFu_C = 0.9975;
-    _CFu_D = 0.0025;
+    // _CFu_A = 0.9950;
+    // _CFu_B = 0.0050;
+    // _CFu_C = 0.9975;
+    // _CFu_D = 0.0025;
 
     // _CFu_A = 0.8182;
     // _CFu_B = 0.1818;
