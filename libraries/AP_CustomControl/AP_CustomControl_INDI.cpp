938426d5eb60403a9fb56f8262e2baa5cb8fd98f
yvltc <joaopeixoto30@gmail.com>
2025-04-16 03:08:59 +0100

    debug

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -443,7 +443,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     else
     {
         // código de motorizado
-        u.z = u_0.z + du.z;
+        u.z = 0*u_0.z + du.z;
         // estabiliza só com du, mas no sítio errado
 
         snprintf(buffer, sizeof(buffer), "u_0: %.6f %.6f %.6f", u_0.x, u_0.y, u_0.z);
