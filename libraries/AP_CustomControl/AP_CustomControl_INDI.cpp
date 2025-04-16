399f306500a7ea4281b8cdc6b71a56f5be660ebe
yvltc <joaopeixoto30@gmail.com>
2025-04-16 02:59:38 +0100

    debug

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -439,7 +439,6 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     {
         // código de planador
         u.z = 0;
-        printf("boas\n");
     }
     else
     {
@@ -447,8 +446,8 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
         u.z = u_0.z + du.z;
         // estabiliza só com du, mas no sítio errado
 
-        // snprintf(buffer, sizeof(buffer), "u_0: %.6f %.6f %.6f", u_0.x, u_0.y, u_0.z);
-        // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
+        snprintf(buffer, sizeof(buffer), "u_0: %.6f %.6f %.6f", u_0.x, u_0.y, u_0.z);
+        gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
         // snprintf(buffer, sizeof(buffer), "du: %.6f %.6f %.6f", du.x, du.y, du.z);
         // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
         // snprintf(buffer, sizeof(buffer), "u: %.6f %.6f %.6f", u.x, u.y, u.z);
