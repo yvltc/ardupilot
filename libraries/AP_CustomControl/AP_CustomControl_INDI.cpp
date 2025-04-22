acc8ddc7abb9c2d2352a629e7a49f378b9461186
yvltc <joaopeixoto30@gmail.com>
2025-04-22 18:29:32 +0100

    debug

--- libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
+++ libraries/AP_CustomControl/AP_CustomControl_INDI.cpp
@@ -470,7 +470,7 @@ void AP_CustomControl_INDI::update(float roll_target, float pitch_target)
     // snprintf(buffer, sizeof(buffer), "xCF: %.6f %.6f %.6f", xCF.x, xCF.y, xCF.z);
     // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
     // command filter
-    // sspace(u, xCF, CF_A, CF_B, CF_C, CF_D, &u, &xCF);
+    sspace(u, xCF, CF_A, CF_B, CF_C, CF_D, &u, &xCF);
 
     // snprintf(buffer, sizeof(buffer), "uCF: %.6f %.6f %.6f", u.x, u.y, u.z);
     // gcs().send_text(MAV_SEVERITY_INFO, "%s", buffer);  // Send the formatted message
