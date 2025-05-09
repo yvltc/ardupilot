6437b30bf33a6ed479df5a6a97e8159f7c7904b7
yvltc <joaopeixoto30@gmail.com>
2025-05-09 16:11:02 +0100

    bump

--- libraries/SITL/SIM_Plane.cpp
+++ libraries/SITL/SIM_Plane.cpp
@@ -56,7 +56,7 @@ Plane::Plane(const char *frame_str) :
     }
     if (strstr(frame_str, "-elevon")) {
         elevons = true;
-        custom_dynamics = true;
+        // custom_dynamics = true;
     } else if (strstr(frame_str, "-vtail")) {
         vtail = true;
     } else if (strstr(frame_str, "-dspoilers")) {
